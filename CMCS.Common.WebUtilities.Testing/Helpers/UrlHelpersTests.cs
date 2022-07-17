5fbede8edc1f40ffce3267aa1bb9c27a700927d0
Carl Molyneux <carl@cm-cs.co.uk>
2022-07-17 19:51:18 +0100

    Added UrlHelpers Encode method

---
+++ CMCS.Common.WebUtilities.Testing/Helpers/UrlHelpersTests.cs
@@ -0,0 +1,54 @@
+﻿using CMCS.Common.WebUtilities.Helpers;
+using CMCS.Common.WebUtilities.Objects;
+using CMCS.Common.WebUtilities.RedirectRules;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.Net.Http.Headers;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace CMCS.Common.WebUtilities.Testing.Helpers
+{
+
+    public class UrlHelpersTests
+    {
+        
+
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void ConvertsToLowerCase()
+        {
+            //Arrange
+            string valueUnderTest =  "AStRiNgWiThCaPiTaLlEtTeRs";
+
+            //Act
+            string result = UrlHelpers.Encode(valueUnderTest);
+
+            //Assert
+            Assert.AreEqual("astringwithcapitalletters", result);
+        }
+
+        [Test]
+        public void ReplacesNonAlphaNumericCharactersWithAHyphen()
+        {
+            //Arrange
+            string valueUnderTest = "A St!RiNg Wi+Th CaPi_TaL lEt%TeRs";
+
+            //Act
+            string result = UrlHelpers.Encode(valueUnderTest);
+
+            //Assert
+            Assert.AreEqual("a-st-ring-wi-th-capi-tal-let-ters", result);
+        }
+
+
+    }
+}
---
+++ CMCS.Common.WebUtilities/Helpers/UrlHelpers.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CMCS.Common.WebUtilities.Helpers
+{
+    public class UrlHelpers
+    {
+        public static string Encode(string value)
+        {
+            string workingValue = value;
+            workingValue = workingValue.Trim();
+            Regex replaceRegEx = new Regex("[^A-Za-z0-9-]");
+            workingValue = replaceRegEx.Replace(workingValue, "-");
+
+            return workingValue.ToLower();
+        }
+    }
+}
