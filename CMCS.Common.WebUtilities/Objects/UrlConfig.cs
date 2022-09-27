ba9c210b0f45674c213cfda24435bb68df28618c
Carl Molyneux <carl@cm-cs.co.uk>
2022-09-27 11:20:10 +0100

    Added methods to return urls

--- CMCS.Common.WebUtilities/Objects/UrlConfig.cs
+++ CMCS.Common.WebUtilities/Objects/UrlConfig.cs
@@ -11,5 +11,27 @@ namespace CMCS.Common.WebUtilities.Objects
         public int? HttpPort { get; set; }
 
         public int? HttpsPort { get; set; }
+
+        public string GetSecureCanonicalUrl()
+        {
+            if(HttpsPort.HasValue)
+            {
+                if (HttpsPort.Value == 443)
+                    return $"https://{CanonicalHost}";
+                return $"https://{CanonicalHost}:{HttpsPort}";
+            }
+            throw new ArgumentNullException("Https port is not set. Cannot provide secure Url");
+        }
+
+        public string GetCanonicalUrl()
+        {
+            if (HttpPort.HasValue)
+            {
+                if (HttpPort.Value == 80)
+                    return $"https://{CanonicalHost}";
+                return $"https://{CanonicalHost}:{HttpPort}";
+            }
+            throw new ArgumentNullException("Http port is not set. Cannot provide Url");
+        }
     }
 }
